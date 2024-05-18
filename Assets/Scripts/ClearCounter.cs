e6eafd127833e4db8c546a9f427e7f4e3f4d4122
Technoo1 <1021945@student.sae.edu.au>
2024-05-18 15:36:16 +0800

    Update ClearCounter.cs

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -13,8 +13,6 @@ public class ClearCounter : MonoBehaviour, IKitchenObjectParent
     private KitchenObject kitchenObject;
 
 
-  
-
     public void Interact(Player player)
     {
         if (kitchenObject == null)
