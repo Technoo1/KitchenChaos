8fd4414d93dd11d3ee13e4c00458b7c94a9b4af9
Technoo1 <1021945@student.sae.edu.au>
2024-06-18 21:14:12 +0800

    SIngleplayer finished

--- Assets/Scripts/IKitchenObjectParent.cs
+++ Assets/Scripts/IKitchenObjectParent.cs
@@ -2,21 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IKitchenObjectParent
-{
-    public Transform GetKitchenObjectFollowTransform();
+public interface IKitchenObjectParent {
 
+    public Transform GetKitchenObjectFollowTransform();
 
     public void SetKitchenObject(KitchenObject kitchenObject);
 
-
     public KitchenObject GetKitchenObject();
-
 
     public void ClearKitchenObject();
 
-
     public bool HasKitchenObject();
+
 }
-
-
